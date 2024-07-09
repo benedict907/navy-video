e495337268091d33b2a0b858168d3e51b765dff0
benedict <benedict@qburst.com>
2024-07-09 11:07:01 +0530

    changed UI for different build

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,38 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
-
+import { Link } from 'react-router-dom'
 const Details = () => {
-  const videoList = ['', '', '', '', '', '', '', '', '']
+  const [videoClicked, setVideoClicked] = useState({
+    title: '01. Introduction',
+    url: 'assets/module_1.mp4',
+    thumbnail: 'assets/thumbnail_1.svg'
+  })
+  const [playing, setPlaying] = useState(false)
+  const videoList = [
+    {
+      title: '01. Introduction',
+      url: 'assets/module_1.mp4',
+      thumbnail: 'assets/thumbnail_1.svg'
+    },
+    {
+      title: '02.⁠ ⁠What Happened ?',
+      url: 'assets/module_2.mp4',
+      thumbnail: 'assets/thumbnail_2.svg'
+    },
+    {
+      title: '03.⁠ ⁠Finding',
+      url: 'assets/module_3.mp4',
+      thumbnail: 'assets/thumbnail_3.svg'
+    }
+  ]
+  const handleKeyDown = event => {
+    console.log('handleKeyDown', playing)
+    if (event.code === 'Space') {
+      setPlaying(prev => !prev)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown)
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [playing, setPlaying])
+
   return (
     <div
-      style={{ backgroundImage: `url("assets/sliderImages/3.svg")` }}
-      className="h-screen w-screen bg-blue-500 flex p-10 justify-between"
+      style={{
+        backgroundImage: `url("assets/home_page.svg")`,
+        backgroundSize: 'cover', // Change this to 'contain', 'auto', or specific dimensions as needed
+        backgroundRepeat: 'no-repeat', // Ensures the image does not repeat
+        backgroundPosition: 'center' // Centers the image
+      }}
+      className="h-full w-full flex-row px-10 pt-10"
     >
-      {/* <h1>Details Page</h1>
-      <Link to="/">Go to Home</Link> */}
-      {/* <div className="flex w-full"> */}
-      {/* <div className="w-1/2 bg-blue-600"> */}
-      <div className="flex mt-10">
-        <ReactPlayer
-          url="assets/sample_video.mp4"
-          playing={false}
-          loop={false}
-          controls={true}
+      <Link to="/">
+        <img
+          className="w-16 active:opacity-50 rotate-180"
+          src="assets/forward_icon.svg"
+          alt="back-icon"
         />
-
-        {/* </div> */}
-        <div className="flex-col w-1/4 ms-10 overflow-scroll">
+      </Link>
+      <div className="flex justify-between items-center">
+        <div className="w-3/4">
+          <ReactPlayer
+            url={videoClicked.url}
+            playing={playing}
+            width={'100%'}
+            height={'100%'}
+            loop={false}
+            controls={true}
+          />
+          <h1 className="text-white font-badoni text-lg mt-2">
+            {videoClicked.title}
+          </h1>
+        </div>
+        <div className="flex-col w-1/4 ms-10">
           {videoList.map(item => (
-            <div className="border border-gray-50 rounded-lg mt-5 active:opacity-50">
-              <img className="" src="assets/sliderImages/3.svg" />
-              <div className="px-2">
-                <h1 className="text-white">01 Prelude</h1>
+            <div
+              key={item.url}
+              onClick={() => {
+                setPlaying(false)
+                setVideoClicked(item)
+              }}
+              className={`border ${videoClicked.url === item.url ? 'border-yellow-400' : 'border-gray-50'}  rounded-lg mb-10 active:opacity-50`}
+            >
+              <img className="" src={item.thumbnail} />
+              <div className="px-2 flex my-1">
+                <h1 className="text-white text-xs mr-2 font-badoni">
+                  {item.title}
+                </h1>
+                <img className="w-6" src={'assets/play_button.svg'} />
               </div>
             </div>
           ))}
         </div>
       </div>
-      {/* </div> */}
     </div>
   )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -13,56 +13,42 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <div className="flex justify-center flex-col">
-        <div className="w-full h-full">
-          <Slider {...settings}>
-            <div>
-              <img
-                className="w-screen h-screen"
-                src={'assets/sliderImages/1.svg'}
-                alt="Slide 1"
-              />
-            </div>
-            <div>
-              <img
-                className="w-screen h-screen"
-                src="assets/sliderImages/2.svg"
-                alt="Slide 2"
-              />
-            </div>
-            <div>
-              <img
-                className="w-screen h-screen"
-                src="assets/sliderImages/3.svg"
-                alt="Slide 3"
-              />
-            </div>
-          </Slider>
-        </div>
-
-        <div className="absolute flex">
-          <img
-            className="w-1/2 h-full rounded-lg"
-            src="assets/sliderImages/3.svg"
-          />
-          <div className="mx-5">
-            <h1 className="text-white font-bold text-4xl">
-              Declasified BOI of IN 240
-            </h1>
-            <h1 className="text-white font-semibold text-lg">
-              Established in 1992, Stratford Accounting is a full service firm,
-              offering affordable accounting solutions to individuals and local
-              and international businesses
-            </h1>
-            <div className="flex justify-end mt-5">
-              <Link to="/details">
-                <button className="bg-yellow-200 p-2 w-32 rounded-full text-black font-bold text-lg">
-                  Start
-                </button>
-              </Link>
-            </div>
+    <div
+      style={{
+        backgroundImage: `url("assets/home_page.svg")`,
+        backgroundSize: 'cover', // Change this to 'contain', 'auto', or specific dimensions as needed
+        backgroundRepeat: 'no-repeat', // Ensures the image does not repeat
+        backgroundPosition: 'center' // Centers the image
+      }}
+      className="h-screen w-full p-10"
+    >
+      <div className="min-h-screen flex flex-col justify-between">
+        <div>
+          <h1 className="text-white text-4xl font-bold text-center mt-20 mb-5 font-badoni">
+            THE AIRBORNE TACTICIANS
+          </h1>
+          <h1 className="text-yellow-300 font-bold text-2xl text-center font-badoni">
+            NAVAL AIR OPERATIONS SCHOOL
+          </h1>
+          <div className="flex justify-center">
+            <img className="w-32" src="assets/logo.svg" alt="Logo" />
           </div>
+          <h1 className="text-white text-3xl font-bold text-center font-badoni">
+            SEAKING 42B BOI
+          </h1>
+        </div>
+        <div className="flex justify-between items-center mb-10 p-4">
+          <div>
+            <h1 className="text-white text-lg font-badoni">Project By</h1>
+            <h1 className="text-white text-lg font-badoni">16 QNI</h1>
+          </div>
+          <Link to="/details">
+            <img
+              className="w-16 active:opacity-50"
+              src="assets/forward_icon.svg"
+              alt="Forward Icon"
+            />
+          </Link>
         </div>
       </div>
     </div>
